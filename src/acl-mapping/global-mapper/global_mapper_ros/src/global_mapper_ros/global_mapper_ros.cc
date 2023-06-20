90f89371fd64bb67dfa6b4590c16b872ad0e447c
sohaila-ahmed3011~ <sohaila.ahmed3011@gmail.com>
2023-06-20 12:12:27 +0000

    clean commit

--- src/acl-mapping/global-mapper/global_mapper_ros/src/global_mapper_ros/global_mapper_ros.cc
+++ src/acl-mapping/global-mapper/global_mapper_ros/src/global_mapper_ros/global_mapper_ros.cc
@@ -29,11 +29,11 @@
 #include "global_mapper/params.h"
 
 ///////////////////////////////////////
-// #include <pcl_conversions/pcl_conversions.h>
-// #include <pcl/point_types.h>
-// #include <pcl/PCLPointCloud2.h>
-// #include <pcl/conversions.h>
-// #include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
 //////////////////////////////////////
 
 namespace global_mapper_ros
